5b98b31731f5bbd05d70f7729a38b4d2befa98a3
An-Shiang Tsai <ast850328@gmail.com>
2017-06-25 12:24:40 +0800

    add script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+var App = (function() {
+	
+	function init() {
+		$('#btn-addTask').click(add);
+		$('#taskContainer').on('click', '.close.btn-delTask', del);
+	}
+
+	function add() {
+		var task = $('#input-taskName').val();
+		var decs = $('#input-taskDecs').val();
+		var show = `
+				<div class="col-sm-4 taskItem">
+					<div class="card">
+						<div class="card-block">
+							<button type="button" class="close btn-delTask" aria-label="Close">
+								<span aria-hidden="true">&times;</span>
+							</button>
+								<h3 class="card-title">${task}</h3>
+								<p class="card-text">${task}</p>
+						</div>
+					</div>
+				</div>`;
+			$('#taskContainer').append(show);
+	}
+
+	function del() {
+		$(this).parent().parent().parent().remove();
+	}
+	return {
+		init: init
+	}
+})();
